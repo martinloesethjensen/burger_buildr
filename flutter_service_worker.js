fd354a8f6d8174908d495d35ebd4162ba78ed8c5
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-02-27 22:47:38 +0100

    Built web
    Branch: main
    Commit: b2905047d37a4ae61d400f84bf8c73da2d34a028
    
    package:peanut 3.7.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bcde17d4188601acb5a365e193685824",
-"index.html": "d2317ad990a232ebe40510d0742b4a2c",
-"/": "d2317ad990a232ebe40510d0742b4a2c",
+"index.html": "c0ee3c289b5ab3545fa447b161d97209",
+"/": "c0ee3c289b5ab3545fa447b161d97209",
 "main.dart.js": "ec716f7b177a1d55090b02e9d6985925",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
